ff7bdd144847e82ed21cc89bb8409414b16ebabd
Petra Gunčić <pguncic@gmail.com>
2023-02-11 23:36:59 +0100

    Refactor Scaledrone integration

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
 import './style.css';
 import getRandomUsername from './helpers/get-random-username.js';
-import createApi from './create-api.js';
 import MessageList from './components/message-list.js';
 import MessageForm from './components/message-form.js';
 
+const drone = new Scaledrone('DOKxbdrSiehFAXBb');
+const room = drone.subscribe('chat_room');
 const appContainer = document.querySelector(".app");
 const currentUser = getRandomUsername();
+const messages = [];
 
 const messageList = new MessageList({
     currentUser: currentUser,
-    messages: []
+    messages: messages
 });
 const messageForm = new MessageForm({
     isLoading: true,
     onNewMessage: newMesageText => {
-        api.sendMessage({
-            author: currentUser,
-            text: newMesageText
+        drone.publish({
+            room: 'chat_room',
+            message: {
+                author: currentUser,
+                text: newMesageText
+            }
         });
     }   
 });
 
 appContainer.append(messageList.htmlElement);
 appContainer.append(messageForm.htmlElement);
-
-const api = createApi({
-    onReady() {
-        messageForm.updateProp('isLoading', false);
-    },
-    onMessagesUpdate(updatedMessages) {
-        messageList.updateProp('messages', updatedMessages);
-    }
+  
+room.on('message', message => {
+    // Received a message sent to the room
+    messages.push(message.data);
+    messageList.updateProp('messages', messages);
 });
